a07ccc004e3f0922dcc2d81314e0bf0312d8fe93
Paul Steven <paul@mediakitchen.co.uk>
2023-04-25 09:22:10 +0100

    Add information content elements

--- src/models/SessionConfig.ts
+++ src/models/SessionConfig.ts
@@ -1,5 +1,6 @@
 import HotspotPage from "./HotspotPage";
 import Link from "./Link";
+import Information from "./Information";
 import Stage from "./Stage";
 import StageAsset from "./StageAsset";
 import StageBackground from "./StageBackground";
@@ -7,6 +8,7 @@ import StageBackground from "./StageBackground";
 export default class SessionConfig {
   public title = "";
   public editorOn = false;
+  public info = new Information();
   public overview = new Link();
   public stage = new Stage();
   public stageBackground = new StageBackground();
